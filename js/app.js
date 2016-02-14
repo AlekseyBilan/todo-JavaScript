7705ef4dcda60fc789b46118f837542041aae79c
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2016-02-15 00:35:15 +0200

    add app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-/**
- * Created by Home2 on 06.02.2016.
- */
+(function () {
+    'use strict';
+
+    /**
+     * Sets up a brand new Todo list.
+     *
+     * @param {string} name The name of your new to do list.
+     */
+    function Todo(name) {
+        this.storage = new app.Store(name);
+        this.model = new app.Model(this.storage);
+        this.template = new app.Template();
+        this.view = new app.View(this.template);
+        this.controller = new app.Controller(this.model, this.view);
+    }
+
+    var todo = new Todo('todos-vanillajs');
+
+    function setView() {
+        todo.controller.setView(document.location.hash);
+    }
+    window.addEventListener('load', setView);
+    window.addEventListener('hashchange', setView);
+})();
